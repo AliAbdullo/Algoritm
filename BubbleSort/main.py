7c38610a435d02e7b77221da7ad6a87ca0745ecc
AliAbdulloh <74522999+AliAbdullo@users.noreply.github.com>
2022-06-07 12:24:44 +0000

    Merge Sort

--- BubbleSort/main.py
+++ BubbleSort/main.py
@@ -5,9 +5,9 @@ def bubbleSort(array):
     for i in range(n):
         for j in range(0,n-i-1):
             if array[j] > array[j+1]:
-                 print(f"{array[j]}, {[array[j+1]]}")
+                 #print(f"{array[j]}, {[array[j+1]]}")
                  array[j],array[j+1]=array[j+1],array[j]
-
+            print(array)
 
 array3 = list(range(39))
 array1=list(range(40))
